8e08a60602f38b4801f69ed9226e44c797499669
Faith Etornam <faithgbadegbe1@gmail.com>
2026-01-29 22:56:11 +0000

    Added addItem function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
-const itemList = document.getElementById('item-list')
+const itemList = document.getElementById("item-list");
+
+function addItem(event) {
+  event.preventDefault();
+  if (itemInput.value === "") {
+    alert("Please add an item");
+    return;
+  }
+  
+}
+
+itemForm.addEventListener("submit", addItem);
